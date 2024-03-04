4660ed09047bc00c904901b3d22e91e3e7d5fb20
Ian <ian.c.bacher@gmail.com>
2024-03-04 16:07:57 -0500

    Initial scripts now load, though Liquibase still fails

--- data/mysql/zz_02_truncate_drugs.sql
+++ data/mysql/zz_02_truncate_drugs.sql
@@ -6,6 +6,20 @@
 
 USE openmrs;
 
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `truncate_if_exist`$$
+
+CREATE  PROCEDURE `truncate_if_exist`(IN tbl_name VARCHAR(150) )
+  BEGIN
+    IF EXISTS( SELECT 1 FROM information_schema.TABLES WHERE able_name = tbl_name AND table_schema = DATABASE()) THEN
+    SET @query = CONCAT('TRUNCATE ', tbl_name);
+    PREPARE stmt FROM @query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+    END IF;
+  END $$
+DELIMITER ;
+
 SET FOREIGN_KEY_CHECKS=0;
-TRUNCATE TABLE `drug` ;
+CALL truncate_if_exist(`drug`);
 SET FOREIGN_KEY_CHECKS=1;                                     
